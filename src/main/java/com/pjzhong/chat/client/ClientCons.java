9dfb0a1399b429939f58df7e80a15a86cd52355c
pj_zhong <pj_zhong@163.com>
2019-08-24 01:04:38 +0800

    Reconnect try no limit

--- src/main/java/com/pjzhong/chat/client/ClientCons.java
+++ src/main/java/com/pjzhong/chat/client/ClientCons.java
@@ -2,9 +2,8 @@ package com.pjzhong.chat.client;
 
 public final class ClientCons {
 
-  public static final int RECONNECT_TYPE = 5;
+  public static final int RECONNECT_TYPE = Integer.MAX_VALUE;
   public static final long RECONNECT_INTERVAL = 3000;
-
 
   private ClientCons() {
   }
